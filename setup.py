5c4abc737a9a9e8e05beca36d3d17ca73ef93a02
parassharmaa <paraazz@outlook.com>
2017-07-16 23:32:19 +0530

    [ADD] data_exploration notebook added

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
-
+requirements = open(os.path.join(here, 'requirements.txt')).read().split('\n')
 
 name = "cortex"
 version = '0.0.1'
 
-install_requires = [
-
-]
+install_requires = requirements
 
 
 setup(
