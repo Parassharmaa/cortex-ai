954647d037afef0642a314dcec46648d7bdefdc9
parassharmaa <paraazz@outlook.com>
2017-08-01 20:08:38 +0530

    [UPDATE] bio fetch script created

---
+++ src/cortex/bio-classifier/_data/fetch_bio.py
@@ -0,0 +1,81 @@
+import tweepy
+import os
+import time
+import pymongo
+
+consumer_key = os.environ['TW_CKEY']
+consumer_secret = os.environ['TW_CSECRET']
+
+access_token = os.environ['TW_ATOKEN']
+access_secret = os.environ['TW_ASECRET']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+db_url = os.environ['BIO_CLASSIFY']
+client = pymongo.MongoClient(db_url)
+
+api = tweepy.API(auth)
+
+reference_users = ["paraazz", 
+					"vmesel",
+					"bansaladitya209",
+					"mitsuhiko",
+					"Kailash26558592",
+					"arorakanav11",
+					"chiax",
+					"jk_rowling",
+					"rishibagree",
+					"rishabhmhjn",
+					"GavinFree",
+					"HerrBains",
+					"JulianAssange",
+					"davidfrawleyved",
+					"DaveLeeBBC",
+					"marcorubio",
+					"CaseyNewton",
+					"narendramodi",
+					"fgautier26",
+					"sundarpichai",
+					"Pogue",
+					"SushmaSwaraj",
+					"waltmossberg"
+					]
+
+time_instance = time.time()
+
+def save_data(data):
+    with open("unlabled/bio_{}.text".format(time_instance), "a") as f:
+    	f.write(data+"\n")
+    	f.close()
+
+i = 0
+total_bio = 0
+t0 = time.time()
+while i < len(reference_users):
+	ids = []
+	print("Getting friends of: ", reference_users[i])
+
+	try:
+		for page in tweepy.Cursor(api.friends_ids, \
+								screen_name=reference_users[i]).pages():
+			print("-")
+			ids.extend(page)
+		print("Total friends:", len(ids))
+		for e in ids:
+			user = api.get_user(e)
+			print("Saving Bio:", user.screen_name)
+			if len(user.description) > 3:
+				total_bio+=1
+				save_data(user.description)
+		
+		i+=1
+	except Exception as e:
+		print("Error: ", e)
+		print("\n\nRetrying in 15 minutes")
+		time.sleep(915)
+		pass
+
+print("Total bio collected:", total_bio)
+print("Time Taken:", time.time()-t0)
+	
