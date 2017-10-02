c44f45377861473147aec82dd113fa552fd88aa9
paras <mail2paras.s@gmail.com>
2017-10-02 23:59:45 +0530

    [UPDATE] serverless lamda

--- src/lambda.py
+++ src/lambda.py
@@ -1,11 +1,15 @@
 import firefly
 
 from cortex.tcategorizer.predict import Classifier
+from cortex.hashtag.hthink import Hthinker
+from cortex.hashtag.word_extract import WordExtractor
 
 imodel_dir = 'cortex/tcategorizer/trained_results_1502219150/'
 imodel_seq = 70
 
 iclf = Classifier(imodel_dir, seq_len = imodel_seq)
+hthink = Hthinker()
+we = WordExtractor()
 
 def predict_interest(payload):
 	result = {}
@@ -13,8 +17,10 @@ def predict_interest(payload):
 		result[s] = iclf.predict(s)
 	return result
 
-def predict_sentiment(payload):
-	result = {}
-	for s in payload:
-		result[s] = sclf.predict(s)
-	return result
+def predict_hashtag(payload):
+	words = we.get_words(payload)
+	hashtags = hthink.think(words)
+	data = {}
+	data['words'] = words
+	data['hashtags'] = hashtags
+	return data
